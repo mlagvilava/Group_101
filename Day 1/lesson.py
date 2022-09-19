4e91e185987f5f5081ff4a902050908d886e2a3f
mlagvilava <mamuka921@gmail.com>
2022-09-20 01:19:46 +0400

    first commit

---
+++ Day 1/lesson.py
@@ -0,0 +1,21 @@
+name = "mamuka" #string tipis cvlad
+age = 30 #int tipis cvladi (mteli ricxvi - integer)
+height = 175.4 #float tipis cvladi (float - atwiladi)
+knows_programming = False #bool tipis cvladi (boolean - logikuri cvladi 2-magi mnishvnelobis)
+is_stading_programing = True #bool 
+surname = "lagvilava"
+#name aris cvladi
+#= aris cvladis inicializacia
+#"mamuka" aris cvladis mnishvneloba
+
+print(name)
+print("mamuka")
+
+#print(type(name))
+#print(type(age))
+#print(type(height))
+#print(type(knows_programming))
+#print(type(is_stading_programing))
+
+print(name + " " + surname)
+print(name + " " + str(age))
