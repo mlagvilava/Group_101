bb0179009660c883cad88c39b722b324a156db02
mlagvilava <mamuka921@gmail.com>
2022-09-20 01:54:12 +0400

    first commit

---
+++ Day 1/homework.py
@@ -0,0 +1,21 @@
+#homework day 1 - Make 10 varualbe about you & print.
+name = "mamuka"
+surname = "lagvilava"
+age = 30
+height = 174.6
+goal = "learn programming"
+skills = "IT support, CNC machine operator"
+lang = "Georgian - origin, russian, english A1"
+hobby = "playng piano, woodcarwing and little gaming"
+all_this_is = True
+
+
+print("Name: " + name)
+print("Surname: " + surname)
+print("Age: " + str(age))
+print("Height: " + str(height))
+print("Goal: " + goal)
+print("Skills: " + skills)
+print("Languages: " + lang)
+print("Hobby: " + hobby)
+print("This is a: " + str(all_this_is))
