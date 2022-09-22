d32ada0870d27364daa01751bfffe3592fe97573
mlagvilava <mamuka921@gmail.com>
2022-09-22 14:56:30 +0400

    first commit

--- Day 1/homework.py
+++ Day 1/homework.py
@@ -10,12 +10,23 @@ hobby = "playng piano, woodcarwing and little gaming"
 this_is = True
 
 
-print("Name: " + name)
-print("Surname: " + surname)
-print("Age: " + str(age))
-print("Height: " + str(height))
-print("Goal: " + goal)
-print("Skills: " + skills)
-print("Languages: " + lang)
-print("Hobby: " + hobby)
-print("This is: " + str(this_is))
+#print("Name: " + name)
+#print("Surname: " + surname)
+#print("Age: " + str(age))
+#print("Height: " + str(height))
+#print("Goal: " + goal)
+#print("Skills: " + skills)
+#print("Languages: " + lang)
+#print("Hobby: " + hobby)
+#print("This is: " + str(this_is))
+
+#ერთ სტრიქონში
+print("About me:")
+print("Hi, I am " + name + " " + surname + ". "  + str(age) + "years old, " + str(height) + " cm tall.")
+print("I want to " + goal + ".")
+print("Skills I have:" + skills)
+print("My hobbies: " + hobby + ".")
+print("Not Final version")
+#It is true that I like the course.
+
+#Hi, I am Mamuka Lagvilava, 30 years old, 185.5 cm tall. I want to learn programming. Skills I have: IT suppot, CNC machine operator. My hobbies: playing the piano, wood carving and a little gaming.
