ad13bc9806dbefd1138d3b805c77f8793f36cc8d
mlagvilava <mamuka921@gmail.com>
2022-09-22 15:46:35 +0400

    first commit

--- Day 1/homework.py
+++ Day 1/homework.py
@@ -7,26 +7,16 @@ goal = "learn programming"
 skills = "IT support, CNC machine operator"
 lang = "Georgian - origin, russian, english A1"
 hobby = "playng piano, woodcarwing and little gaming"
-this_is = True
+true = True
 
 
-#print("Name: " + name)
-#print("Surname: " + surname)
-#print("Age: " + str(age))
-#print("Height: " + str(height))
-#print("Goal: " + goal)
-#print("Skills: " + skills)
-#print("Languages: " + lang)
-#print("Hobby: " + hobby)
-#print("This is: " + str(this_is))
-
-#ერთ სტრიქონში
+print("")
 print("About me:")
+print("")
 print("Hi, I am " + name + " " + surname + ". "  + str(age) + "years old, " + str(height) + " cm tall.")
 print("I want to " + goal + ".")
 print("Skills I have:" + skills)
 print("My hobbies: " + hobby + ".")
-print("Not Final version")
-#It is true that I like the course.
-
-#Hi, I am Mamuka Lagvilava, 30 years old, 185.5 cm tall. I want to learn programming. Skills I have: IT suppot, CNC machine operator. My hobbies: playing the piano, wood carving and a little gaming.
+print("Now I'm taking a programming course at Goal oriented academy")
+print("It is " + str(true) + " that I like the course." )
+print("")
