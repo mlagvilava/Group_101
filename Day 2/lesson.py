ecfc4fed684f4cfd2e16a8b38b0dbcb05d01a867
mlagvilava <mamuka921@gmail.com>
2022-09-22 23:31:43 +0400

    first commit

--- Day 2/lesson.py
+++ Day 2/lesson.py
@@ -3,3 +3,33 @@ age = 30
 height = 174.6
 
 print("Chemi saxelia {} da asaki {}".format(name, age))
+
+full_name = "mamuka lagvilava"
+print(full_name[4:9])
+
+
+
+name2 = "mamuka"
+print(len(name2))
+
+
+if "a" in full_name:
+    print("gvxvdeba")
+
+
+print(full_name[2:])
+print(full_name[:6])
+print(full_name[::3])
+
+
+print(full_name[-1])
+
+print(full_name.upper())
+
+full_name2 = "MaMUKA LaGVILAVA"
+print(full_name2.lower())
+full_name3 = "     1 mamuka 2    "
+print(full_name3.strip())
+
+name = "nika"
+print(name.replace("", "#"))
