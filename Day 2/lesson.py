9bdc05eee1ce8b9f2476b67018f6919b8c3a9711
mlagvilava <mamuka921@gmail.com>
2022-09-24 11:49:23 +0400

    first commit

--- Day 2/lesson.py
+++ Day 2/lesson.py
@@ -1,12 +1,28 @@
-name = "mamuka."
-age = 30
-height = 174.6
+name = "mamuka."    #string type varuable
+age = 30            #int type varuable
+height = 174.6      #float type varuable
+true = True         #boolean type varuable
 
-print("Chemi saxelia {} da asaki {}".format(name, age))
+
+#if არის ლოგიკური ალტერნატივა პროგრამირებაში.
+#თუ if ის მარჯვნივ დაწერილი პირობა True (ჭეშმარიტი) - ა მაშინ ქვევით დაწერილი კოდი შესრულდება.
+#False (მცდარი) - ს შემთხვევაში არ შესრულდება.
+if true:
+    print("Say_Yes") #print_ის წინ ტაბია = 4 Space - ს. ამ გამოყოფას ქვია indentation.
+
+if 5>10:            #ამ შემთხვევაში პირობა მცდარია
+    print("_:(_")  #შესაბამისად ეს კოდიც არ შესრულდება.
+
+
+
+
+
+print("Chemi saxelia {} da asaki {}".format(name, age)) #   
+#Chemi Saxelia, da saki  ობიექტებია .format კი მეთოდი
+#ხოლო  name და surname არგუმნტები, რომლებიც format მეთოდს გადაეცა.                                                  #
 
 full_name = "mamuka lagvilava"
 print(full_name[4:9])
-
 
 
 name2 = "mamuka"
