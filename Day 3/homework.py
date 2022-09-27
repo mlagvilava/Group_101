4d67914e6177cd9124d1156e6c139252128ac942
mlagvilava <mamuka921@gmail.com>
2022-09-27 11:54:57 +0400

    first commit

--- Day 3/homework.py
+++ Day 3/homework.py
@@ -5,21 +5,17 @@ num1 = int(input("First number"))
 num2 = int(input("Second number"))
 num3 = int(input("Third number"))
 
-num_1 = num1 % 2
-num_2 = num2 % 2
-num_3 = num3 % 2
-
-if num_1 > 0:
+if num1 % 2 > 0:
     num1 = num1
 else:
     num1 = 0
 
-if num_2 > 0:
+if num2 % 2 > 0:
     num2 = num2
 else:
     num2 = 0
 
-if num_3 > 0:
+if num3 % 2 > 0:
     num3 = num3
 else:
     num3 = 0
