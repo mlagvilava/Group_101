7de1db8db3cc9be4fc97b444b795cc559e509e6a
mlagvilava <mamuka921@gmail.com>
2022-10-17 11:27:17 +0400

    first commit

--- Day 7/homework.py
+++ Day 7/homework.py
@@ -1,29 +1,37 @@
-scores = [20, 43, 56, 73, 10, 6, 87, 45, 97]
-high_score = 0
+# scores = [20, 43, 56, 73, 10, 6, 87, 45, 97]
+# high_score = 0
 
-for x in scores:
-    for i in scores:
-        if x > i:
-            high_score = x
-            i = x
-            print(str(x) + " it\'s is x")
-        elif x < i:
-            hight_score = i
-            x = i
-            print(str(i) + " it\'s is i")
+# for x in scores:
+#     for i in scores:
+#         if x > i:
+#             high_score = x
+#             i = x
+#             print(str(x) + " it\'s is x")
+#         elif x < i:
+#             hight_score = i
+#             x = i
+#             print(str(i) + " it\'s is i")
 
-print(str(high_score) + " it\'s literaly hard for me")
+# print(str(high_score) + " it\'s literaly hard for me")
 
 #-----------------------------------------------------
 
+#ორ რიცხვს შორის უდიდესს ანიჭებს max_vol ცვლადს და ცილკის მეშვეობით 
+#აგრძელებს შედარების პროცესს მარჯვნივ მდგომ რიცხვთან.
+#შედარების შედეგი ყოველ ჯერზე გამოაქვს ტერმინალში - თვალსაჩინობისთვის.
 # scores = [20, 43, 56, 73, 10, 6, 87, 45, 97]
-# scor1 = scores[0::2]
-# scor2 = scores[1::2]
+# max_vol = 0
 # i = 0
-# while i < len(scores):
-#     if scores[:i:] > scores[i:2:2]:
-#         print(i)
+# while i+1 < len(scores): #სანამ i+1 არ დავწერე 27-ე ხაზზე აგდებდა შეცდომას Out of range
+#     x = i + 1
+#     if scores[i] > scores[x]:
+#         max_vol = scores[i]
+#         print(str(max_vol) + " Metia " + str(scores[x]) + "-ze")
+#     else:
+#         max_vol = scores[x]
+#         print(str(scores[i]) + " Naklebia " + str(max_vol) + "-ze")
 #     i += 1
-# # print(scores[:1:] < scores[1:2:2])
-# # print(scores[:1:])
-# # print(scores[1:2:2])
+
+#-----------------------------------------------------
+
+students = ["nika", "ana", "mamuka", "beqa", "dachi", "iva", "farna"]
